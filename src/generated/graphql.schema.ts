ad8a7ddc0d28d818a0f44af608247e8b51e10509
nyunwo <nyunwo.w@gmail.com>
2020-04-08 00:13:25 +0800

    no message

--- src/generated/graphql.schema.ts
+++ src/generated/graphql.schema.ts
@@ -6,6 +6,15 @@
 
 /* tslint:disable */
 /* eslint-disable */
+export class UserInput {
+    username: string;
+    password?: string;
+}
+
+export abstract class IMutation {
+    abstract addUser(userData?: UserInput): User | Promise<User>;
+}
+
 export class Person {
     name?: string;
     age?: number;
@@ -14,7 +23,7 @@ export class Person {
 export abstract class IQuery {
     abstract say(): Person | Promise<Person>;
 
-    abstract getUser(): User | Promise<User>;
+    abstract getUser(id?: number): User | Promise<User>;
 }
 
 export class User {
--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -1,15 +1,18 @@
 import { Resolver, Mutation, Args, Query } from '@nestjs/graphql'
+import { Param } from '@nestjs/common'
 import gql from 'graphql-tag'
-import { User } from '../../generated/graphql.schema'
+import { User, UserInput } from '../../generated/graphql.schema'
 import { ApolloClient } from '../../core/decorators/apolloclient.decorator'
+import { fromPromise } from 'apollo-boost'
 
 @Resolver('User')
 export class UserResolver {
     @Query()
-    async getUser(@ApolloClient() client:any): Promise<User> {
+    async getUser(@Args('id') id: Number, @ApolloClient() client:any): Promise<User> {
+        console.log('id:', id)
         const where = gql`
         query user {
-            user {
+            user(where: {id: {_eq: ${id}}}) {
                 id
                 username
             }
@@ -22,5 +25,30 @@ export class UserResolver {
         })
         console.log('result:', result.data)
         return result.data.user[0]
+    }
+
+    @Mutation()
+    async addUser(@Args('userData') userData: UserInput, @ApolloClient() client:any): Promise<User>{
+        console.log('userData', userData)
+        const where = gql`
+        mutation addUser {
+            insert_user(objects:{
+                username: "${userData.username}"
+                password: "${userData.password}"
+            }){
+                returning {
+                    id
+                    username
+                }
+            }
+        }
+        `
+        const result = await client.query({
+            query: where,
+            variables: {},
+            fetchPolicy: 'no-cache'
+        })
+        console.log('result:', result.data.insert_user.returning[0])
+        return result.data.insert_user.returning[0]
     }
 }

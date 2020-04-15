5d492a18665ddf97479fca5de4a05e7157b00145
nyunwo <nyunwo.w@gmail.com>
2020-04-16 00:58:55 +0800

    no message

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,17 +1,23 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm'
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column({ length: 30, unique: true })
     username: string;
 
-    @Column()
+    @Column({ length: 50, nullable: true })
     password: string;
 
-    @Column()
-    age: number;
+    @Column({ length: 30, nullable: true })
+    salt: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
 
 }
--- src/modules/home/home.service.ts
+++ src/modules/home/home.service.ts
@@ -7,8 +7,14 @@ export class HomeService {
         private readonly userService: UserService
     ){}
     async sayHi(){
-        let users = await this.userService.findByUsername('Tom')
-        console.log('users:', users)
+        // let user = await this.userService.findByUsername('Tom')
+        // console.log('users:', user)
+
+        // let r = await this.userService.addUser('Tom')
+        // console.log('r:', r)
+
+        let r = await this.userService.updatePasswd('Tom', '1243')
+        console.log(r)
         return 'Hello Nest'
     }
 }
